e90371d7e4741c0209bd620ff23cd7484ffe42e7
Tauani Vitória <tauanivitoria1302@gmail.com>
2024-05-21 22:05:54 -0300

    Criação do projeto

---
+++ TauBiRy/TauBiRy/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TauBiRy
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TauBiRy/TauBiRy/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TauBiRy
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
