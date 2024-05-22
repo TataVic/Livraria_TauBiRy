64b60ccc66c02091ec42546a125c9b8a9003d118
Bianca Netto <biancanettoc@gmail.com>
2024-05-22 16:03:10 -0300

    Classe livros
    Criação da classe livros e seus campos

---
+++ TauBiRy/TauBiRy/Livros.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace TauBiRy
+{
+    [SQLite.Table("Livros")]
+    public class Livro
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public required string Titulo { get; set; }
+        public required string Autor { get; set; }
+        public required string Isbn { get; set; }
+        public required string Categoria { get; set; } // chave estrangeria da tabela categoria
+        public required string Idioma { get; set; }
+        public required string Editora { get; set; }
+        public required DateTime Anolancamento { get; set; }
+        public string? avalipessoal { get; set; }
+        public string? Lido { get; set; }
+    }
+}
